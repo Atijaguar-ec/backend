2a3670d71c88a601c755cab8ed54c9a76e0cbdcb
Alvaro Sanchez <alvaro.sanchez@atijaguar.com>
2025-10-08 10:04:35 -0500

    SQL inicia UNOCASE

--- ec/init_company_unocace.sql
+++ ec/init_company_unocace.sql
@@ -1,3 +1,5 @@
--- Script para crear la compañía UNOCACE
-INSERT INTO company (name, address, created_at, updated_at)
-VALUES ('UNOCACE', 'Km 30 vía Duran - Boliche - Milagro, Recinto El Deseo - Ecuador', NOW(), NOW());
+INSERT INTO Company (name,entityVersion, headquarters_address)
+VALUES ('UNOCACE',1, 'Km 30 vía Duran - Boliche - Milagro, Recinto El Deseo - Ecuador');
+
+INSERT INTO CompanyUser (company_id, user_id, role,entityVersion)
+VALUES (1, 2, 'COMPANY_ADMIN',1);
