c1e141e8c8cebf298d2bab9e6c7f7480543becaa
Alvaro Sanchez <alvaro.sanchez@atijaguar.com>
2025-10-30 01:19:48 -0500

    Arreglo de la migracion

--- src/main/resources/db/migrations/V2025_10_29_19_35__Add_parcel_variety_organicCert_to_StockOrder.sql
+++ src/main/resources/db/migrations/V2025_10_29_19_35__Add_parcel_variety_organicCert_to_StockOrder.sql
@@ -1,5 +1,4 @@
 -- Add parcel lot, variety and organic certification fields to stock_order table
-
 ALTER TABLE StockOrder ADD COLUMN parcelLot VARCHAR(8);
 ALTER TABLE StockOrder ADD COLUMN variety VARCHAR(32);
 ALTER TABLE StockOrder ADD COLUMN organicCertification VARCHAR(64);
