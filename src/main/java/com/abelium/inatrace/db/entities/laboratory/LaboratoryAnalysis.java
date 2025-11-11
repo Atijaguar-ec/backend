121d8b288bfaa593ea374aa8d05eb09cf6bf6854
Alvaro Sanchez <alvaro.sanchez@atijaguar.com>
2025-11-11 15:18:09 -0500

    refactor: migrate LaboratoryAnalysis to Jakarta Persistence API

--- src/main/java/com/abelium/inatrace/db/entities/laboratory/LaboratoryAnalysis.java
+++ src/main/java/com/abelium/inatrace/db/entities/laboratory/LaboratoryAnalysis.java
@@ -4,7 +4,7 @@ import com.abelium.inatrace.db.base.TimestampEntity;
 import com.abelium.inatrace.db.entities.common.User;
 import com.abelium.inatrace.db.entities.stockorder.StockOrder;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.time.Instant;
 
 /**
